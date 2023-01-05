cdb1f20043561f5a6d587104f6fc71ad91e295c4
Murat Altun <mraltun@gmail.com>
2023-01-05 12:46:18 +0300

    Cache prisma client in db file

---
+++ lib/db.ts
@@ -0,0 +1,20 @@
+// Because Next api functions run in a serveless environment, we're going to cache our prisma client and reuse it when possible to avoid having too many connections.
+import { PrismaClient } from "@prisma/client";
+
+// TS
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
+  }
+  prisma = global.cachedPrisma;
+}
+
+export const db = prisma;
