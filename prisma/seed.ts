adcb06a1e5af0656bef977c5bcf812baf57d597b
Murat Altun <mraltun@gmail.com>
2023-01-05 14:03:19 +0300

    Removed hashpassword from seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,5 @@
 // When building out our UI, we'll need data for our components to render. We can mock this data out on many different levels. Hard code in the components, create a JSON file with fake data, skip the ORM and use fake data, or we can seed the DB. This keeps us from going back and making changes to our code once we actually have real data and need to remove the mocks. All we have to do is write a seed script. A seed script is just a piece of code that inserts fake data into our dev DB so we can use it for development.
 
-import { hashPassword } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { TASK_STATUS } from "@prisma/client";
 
@@ -22,7 +21,7 @@ async function main() {
       email: "user@email.com",
       firstName: "User",
       lastName: "Person",
-      password: await hashPassword("password"),
+      password: "password",
       projects: {
         create: new Array(5).fill(1).map((_, i) => ({
           name: `Project ${i}`,
