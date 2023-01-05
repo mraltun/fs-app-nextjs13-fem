654109f208ae0d4397a1ff461f429001f7730d10
Murat Altun <mraltun@gmail.com>
2023-01-05 13:20:22 +0300

    Added seed file for mock database

---
+++ prisma/seed.ts
@@ -0,0 +1,65 @@
+// When building out our UI, we'll need data for our components to render. We can mock this data out on many different levels. Hard code in the components, create a JSON file with fake data, skip the ORM and use fake data, or we can seed the DB. This keeps us from going back and making changes to our code once we actually have real data and need to remove the mocks. All we have to do is write a seed script. A seed script is just a piece of code that inserts fake data into our dev DB so we can use it for development.
+
+import { hashPassword } from "../lib/auth";
+import { db } from "../lib/db";
+import { TASK_STATUS } from "@prisma/client";
+
+const getRandomTaskStatus = () => {
+  const statuses = [
+    TASK_STATUS.COMPLETED,
+    TASK_STATUS.NOT_STARTED,
+    TASK_STATUS.STARTED,
+  ];
+  return statuses[Math.floor(Math.random() * statuses.length)];
+};
+
+async function main() {
+  // Create fake user. Upsert is update or create if it doesn't exists
+  const user = await db.user.upsert({
+    where: { email: "user@email.com" },
+    update: {},
+    create: {
+      email: "user@email.com",
+      firstName: "User",
+      lastName: "Person",
+      password: await hashPassword("password"),
+      projects: {
+        create: new Array(5).fill(1).map((_, i) => ({
+          name: `Project ${i}`,
+          due: new Date(2022, 11, 25),
+        })),
+      },
+    },
+    include: {
+      projects: true,
+    },
+  });
+
+  const tasks = await Promise.all(
+    user.projects.map((project) =>
+      db.task.createMany({
+        data: new Array(10).fill(1).map((_, i) => {
+          return {
+            name: `Task ${i}`,
+            ownerId: user.id,
+            projectId: project.id,
+            description: `Everything that describes Task ${i}`,
+            status: getRandomTaskStatus(),
+          };
+        }),
+      })
+    )
+  );
+
+  console.log({ user, tasks });
+}
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
