ab226e1098d7fe23a05c28cd9f662227467e21d0
Murat Altun <mraltun@gmail.com>
2023-01-05 13:51:44 +0300

    Added config files for TS

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 // When building out our UI, we'll need data for our components to render. We can mock this data out on many different levels. Hard code in the components, create a JSON file with fake data, skip the ORM and use fake data, or we can seed the DB. This keeps us from going back and making changes to our code once we actually have real data and need to remove the mocks. All we have to do is write a seed script. A seed script is just a piece of code that inserts fake data into our dev DB so we can use it for development.
 
-import { hashPassword } from "../lib/auth";
-import { db } from "../lib/db";
+import { hashPassword } from "@/lib/auth";
+import { db } from "@/lib/db";
 import { TASK_STATUS } from "@prisma/client";
 
 const getRandomTaskStatus = () => {
