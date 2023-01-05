d6231d6791626f6031484d9a7f4312db0b37d237
Murat Altun <mraltun@gmail.com>
2023-01-05 13:06:21 +0300

    Updated db file

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,13 @@
-// Because Next api functions run in a serveless environment, we're going to cache our prisma client and reuse it when possible to avoid having too many connections.
+// Because Next api functions run in a serverless environment, we're going to cache our prisma client and reuse it when possible to avoid having too many connections.
 import { PrismaClient } from "@prisma/client";
 
-// TS
+// TS. Disable eslint because if you try to use something in the global space that's not actually there, TS will freak out.
 declare global {
   // eslint-disable-next-line no-var
   var cachedPrisma: PrismaClient;
 }
 
+// We are going to create prisma client instance and it's going to check if we already had one. If we already did then it gets that one, otherwise save in the global and return that one
 let prisma: PrismaClient;
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
