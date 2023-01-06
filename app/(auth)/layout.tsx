3706b7393e245791682109048aa270298ce5c720
Murat Altun <mraltun@gmail.com>
2023-01-06 13:14:37 +0300

    Updated both auth and dashboard root layouts

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,14 @@
 import GlassPane from "@/components/GlassPane";
+import { Inter } from "@next/font/google";
+import "@/styles/global.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+});
 
 const AuthRootLayout = ({ children }) => {
   return (
-    <html lang='en'>
+    <html lang='en' className={inter.variable}>
       <head />
       <body className='h-screen w-screen rainvox-mesh p-6'>
         <GlassPane className='w-full h-full flex items-center justify-center' />
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+// import Sidebar from "@/components/Sidebar";
+import GlassPane from "@/components/GlassPane";
+import { Inter } from "@next/font/google";
+import "@/styles/global.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+});
+
+const DashboardRootLayout = () => {
+  return (
+    <html lang='en' className={clsx(inter.variable, "dark")}>
+      <head />
+      <body className='h-screen w-screen candy-mesh p-6'>
+        <GlassPane className='w-full h-full p-6 flex align-center container mx-auto'>
+          {/* <Sidebar /> */}
+          <main className='w-full pl-6 h-full'>{children}</main>
+        </GlassPane>
+      </body>
+    </html>
+  );
+};
+
+export default DashboardRootLayout;
