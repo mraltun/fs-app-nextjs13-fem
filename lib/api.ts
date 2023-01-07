e1c989dcbef98e584df4fcd56b30f2afb4f3d2af
Murat Altun <mraltun@gmail.com>
2023-01-07 16:40:51 +0300

    Added AuthForm component

--- lib/api.ts
+++ lib/api.ts
@@ -2,6 +2,7 @@ const fetcher = async ({ url, method, body, json = true }) => {
   const res = await fetch(url, {
     method,
     body: body && JSON.stringify(body),
+    // ...(body && { body: JSON.stringify(body) }),
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
