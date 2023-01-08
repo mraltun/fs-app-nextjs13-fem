8f64ba65fe400330bdc510b040531cd8d9718f0a
Murat Altun <mraltun@gmail.com>
2023-01-08 15:47:56 +0300

    Added JWT methods to auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+// JSON Web Almost Everything
 import { SignJWT, jwtVerify } from "jose";
 import { db } from "./db";
 
@@ -6,3 +7,39 @@ export const hashPassword = (password) => bcrypt.hash(password, 10);
 
 export const comparePasswords = (plainTextPassword, hashedPassword) =>
   bcrypt.compare(plainTextPassword, hashedPassword);
+
+export const createJWT = (user) => {
+  // Expire the token after a week from today
+  const iat = Math.floor(Date.now() / 1000);
+  const exp = iat + 60 * 60 * 24 * 7;
+
+  return new SignJWT({ payload: { id: user.id, email: user.email } })
+    .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+    .setExpirationTime(exp)
+    .setIssuedAt(iat)
+    .setNotBefore(iat)
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+};
+
+export const validateJWT = async (jwt) => {
+  const { payload } = await jwtVerify(
+    jwt,
+    new TextEncoder().encode(process.env.JWT_SECRET)
+  );
+
+  return payload.payload as any;
+};
+
+export const getUserFromCookie = async (cookies) => {
+  const jwt = cookies.get(process.env.COOKIE_NAME);
+
+  const { id } = await validateJWT(jwt.value);
+
+  const user = await db.user.findUnique({
+    where: {
+      id: id as string,
+    },
+  });
+
+  return user;
+};
