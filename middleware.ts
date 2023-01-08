a7497c69ec94bfb651c3d8c4776fb27a2bb85b4c
Murat Altun <mraltun@gmail.com>
2023-01-08 18:05:22 +0300

    Added middleware comments

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 // Middleware has to be on the root with middleware.ts(.js)  name.
 import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
-//
 const PUBLIC_FILE = /\.(.*)$/;
 
+// We had to make this again here as the other one is in a file with bcrypt which is not supported on edge runtimes. Edge environment won't run dynamic code, it has to be static. So most of the packages can run on the nodejs won't run here. That's why we use jose instead of jsonwebtoken package.
 const verifyJWT = async (jwt) => {
   const { payload } = await jwtVerify(
     jwt,
@@ -15,6 +15,7 @@ const verifyJWT = async (jwt) => {
 
 export default async function middleware(req, res) {
   const { pathname } = req.nextUrl;
+  // Because this middleware run in every route we need to tell our routes.
   if (
     pathname.startsWith("/_next") ||
     pathname.startsWith("/api") ||
