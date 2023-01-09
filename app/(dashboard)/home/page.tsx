5f6ac199bfb0ce003e3a09db0ac5b21f8324bce9
Murat Altun <mraltun@gmail.com>
2023-01-09 15:29:26 +0300

    Updated dashboard home with suspense

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,17 +1,20 @@
-import Greeting from "@/components/Greeting";
+import { Suspense } from "react";
+import { cookies } from "next/headers";
+import Link from "next/link";
 import { delay } from "@/lib/async";
 import { getUserFromCookie } from "@/lib/auth";
 import { db } from "@/lib/db";
-import { cookies } from "next/headers";
-import Link from "next/link";
-import { Suspense } from "react";
+import Greeting from "@/components/Greeting";
 
 export default async function Page() {
   return (
     <div className='h-full overflow-y-auto pr-6 w-1/1'>
       <div className=' h-full  items-stretch justify-center min-h-[content]'>
         <div className='flex-1 grow flex'>
-          <Greeting />
+          {/* Suspense lets you display a fallback until its children have finished loading. (We show skeleton while loading Greeting) */}
+          <Suspense fallback={<GreetingsSkeleton />}>
+            <Greeting />
+          </Suspense>
         </div>
         <div className='flex flex-2 grow items-center flex-wrap mt-3 -m-3 '>
           {/** projects map here */}
