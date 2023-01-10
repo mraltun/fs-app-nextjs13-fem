cabb32ab61685cee495b3d58d2e5a9b07dc75742
Murat Altun <mraltun@gmail.com>
2023-01-10 19:05:03 +0300

    Added TS and Eslint ignore errors

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
     appDir: true,
     serverComponentsExternalPackages: ["bcrypt"],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if your project has type errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
